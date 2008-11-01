56589dd426029be9b84314222fc476810c80c064
Ingo Schommer <ingo@silverstripe.com>
2008-11-01 15:31:08 +0000

    MINOR Added language master table

---
+++ lang/en_US.php
@@ -0,0 +1,51 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['Subsite']['PLURALNAME'] = array(
+	'Subsits',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['Subsite']['SINGULARNAME'] = array(
+	'Subsite',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['Subsite']['db_DefaultSite'] = array(
+	'DefaultSite',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Subsite']['db_Domain'] = array(
+	'Domain',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Subsite']['db_IsPublic'] = array(
+	'IsPublic',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Subsite']['db_RedirectURL'] = array(
+	'RedirectURL',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Subsite']['db_Subdomain'] = array(
+	'Subdomain',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Subsite']['db_Theme'] = array(
+	'Theme',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Subsite']['db_Title'] = array(
+	'Title',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+
+?>
