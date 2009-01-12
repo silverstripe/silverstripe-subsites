abc932a114f221b4a40a68ae8a34cf42f28e9db6
Ingo Schommer <ingo@silverstripe.com>
2009-01-12 05:09:57 +0000

    MINOR Updated language master tables

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,15 +2,15 @@
 
 global $lang;
 
+$lang['en_US']['SubsiteAdmin']['MENUTITLE'] = array(
+	'Subsites',
+	100,
+	'Menu title'
+);
 $lang['en_US']['GroupSubsites']['SECURITYACCESS'] = array(
 	'Limit CMS access to subsites',
 	PR_MEDIUM,
 	'Dropdown listing existing subsites which this group has access to'
-);
-$lang['en_US']['GroupSubsites']['SECURITYACCESS_ALL'] = array(
-	'all',
-	PR_MEDIUM,
-	'Default for dropdown selection: Group has access to all existingsubsites'
 );
 $lang['en_US']['GroupSubsites']['SECURITYTABTITLE'] = 'Subsites';
 $lang['en_US']['Subsite']['PLURALNAME'] = array(
@@ -23,5 +23,17 @@ $lang['en_US']['Subsite']['SINGULARNAME'] = array(
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
 );
+$lang['en_US']['SubsitesVirtualPage']['PLURALNAME'] = array(
+	'Subsites Virtual Pags',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['SubsitesVirtualPage']['SINGULARNAME'] = array(
+	'Subsites Virtual Page',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['VirtualPage']['CHOOSE'] = 'Choose a page to link to';
+$lang['en_US']['VirtualPage']['EDITCONTENT'] = 'Click here to edit the content';
 
 ?>
