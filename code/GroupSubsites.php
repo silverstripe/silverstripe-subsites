6b8b07d2a923e7029f5483b90e8211f965a43b30
Hamish Friedlander <hamish@silverstripe.com>
2009-04-07 02:54:10 +0000

    BUGFIX: Adjust Group admin to only allow setting the Subsite of a Group to a Subsite the user has edit permissions on.

--- code/GroupSubsites.php
+++ code/GroupSubsites.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Extension for the Group object to add subsites support
- * 
+ *
  * @package subsites
  */
 class GroupSubsites extends DataObjectDecorator {
-	
+
 	function extraDBFields() {
 		// This is hard-coded to be applied to SiteTree, unfortunately
-        if($this->owner->class == 'Group') {
+		if($this->owner->class == 'Group') {
 			return array(
 				'has_one' => array(
 					'Subsite' => 'Subsite',
@@ -16,38 +16,54 @@ class GroupSubsites extends DataObjectDecorator {
 			);
 		}
 	}
-	
-	function updateCMSFields(&$fields) {
-		$subsites = DataObject::get('Subsite');
-		$tab = $fields->findOrMakeTab(
-			'Root.Subsites',
-			_t('GroupSubsites.SECURITYTABTITLE', 'Subsites')
-		);
-		$tab->push(new DropdownField(
-			'SubsiteID', 
-			_t('GroupSubsites.SECURITYACCESS', 'Limit CMS access to subsites', PR_MEDIUM, 'Dropdown listing existing subsites which this group has access to'),
-			($subsites) ? $subsites->toDropDownMap() : null,
-			null,
-			null,
-			''
-		));
 
+	function updateCMSFields(&$fields) {
+
+		if( $this->owner->SubsiteID == 0 || $this->owner->canEdit() ){
+			$subsites = DataObject::get('Subsite');
+
+			$oldSubsiteID = Session::get('SubsiteID');
+			foreach( $subsites as $subsite ) {
+				Subsite::changeSubsite($subsite->ID);
+				if ( !Permission::check('CL_Admin') ) { $subsites->remove( $subsite ) ; }
+			}
+			Subsite::changeSubsite($oldSubsiteID);
+
+			if ( $subsites->exists() ) {
+				$tab = $fields->findOrMakeTab(
+					'Root.Subsites',
+					_t('GroupSubsites.SECURITYTABTITLE', 'Subsites')
+				);
+
+				$dropdown = new DropdownField(
+					'SubsiteID',
+					_t('GroupSubsites.SECURITYACCESS', 'Limit CMS access to subsites', PR_MEDIUM, 'Dropdown listing existing subsites which this group has access to'),
+					$subsites->toDropDownMap(),
+					null,
+					null,
+					''
+				);
+				if ( $subsites->Count() == 1 ) $dropdown = $dropdown->transform(new ReadonlyTransformation()) ;
+
+				$tab->push($dropdown) ;
+			}
+		}
 	}
-	
+
 	/**
 	 * If this group belongs to a subsite,
 	 * append the subsites title to the group title
 	 * to make it easy to distinguish in the tree-view
 	 * of the security admin interface.
 	 */
-    function alternateTreeTitle() {
-        if($this->owner->SubsiteID == 0) {
+	function alternateTreeTitle() {
+		if($this->owner->SubsiteID == 0) {
 			return $this->owner->Title . ' <i>(global group)</i>';
 		} else {
 			return $this->owner->Title; // . ' <i>(' . $this->owner->Subsite()->Title . ')</i>';
 		}
-    }
-    
+	}
+
 	/**
 	 * Update any requests to limit the results to the current site
 	 */
@@ -59,32 +75,31 @@ class GroupSubsites extends DataObjectDecorator {
 
 			if($context = DataObject::context_obj()) $subsiteID = (int)$context->SubsiteID;
 			else $subsiteID = (int)Subsite::currentSubsiteID();
-			
+
 			// The foreach is an ugly way of getting the first key :-)
 			foreach($query->from as $tableName => $info) {
 				$query->where[] = "`$tableName`.SubsiteID IN (0, $subsiteID)";
 				break;
 			}
 
-            $query->orderby = 'SubsiteID' . ($query->orderby ? ', ' : '') . $query->orderby;
+			$query->orderby = 'SubsiteID' . ($query->orderby ? ', ' : '') . $query->orderby;
 		}
 	}
-	
+
 	function augmentBeforeWrite() {
 		if(!is_numeric($this->owner->ID) && !$this->owner->SubsiteID) $this->owner->SubsiteID = Subsite::currentSubsiteID();
 	}
-	
+
 	function alternateCanEdit() {
 		// Check the CMS_ACCESS_SecurityAdmin privileges on the subsite that owns this group
 		$oldSubsiteID = Session::get('SubsiteID');
 
-		Session::set('SubsiteID', $this->owner->SubsiteID);
+		Subsite::changeSubsite($this->owner->SubsiteID) ;
 		$access = Permission::check('CMS_ACCESS_SecurityAdmin');
-		Session::set('SubsiteID', $oldSubsiteID);
-		
+		Subsite::changeSubsite($oldSubsiteID) ;
+
 		return $access;
 	}
-
 
 	/**
 	 * Create a duplicate of this group and save it to another subsite.
@@ -98,13 +113,13 @@ class GroupSubsites extends DataObjectDecorator {
 		} else {
 			$subsite = DataObject::get_by_id('Subsite', $subsiteID);
 		}
-		
+
 		$group = $this->owner->duplicate(false);
 
 		$subsiteID = ($subsiteID ? $subsiteID : Subsite::currentSubsiteID());
 		$group->SubsiteID = $subsiteID;
 		$group->write();
-		
+
 		// Duplicate permissions
 		$permissions = $this->owner->Permissions();
 		foreach($permissions as $permission) {
