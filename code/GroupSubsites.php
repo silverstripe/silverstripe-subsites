2b70ef13ea3ca04026867faebfb9ec5ccc8bcc3b
Sean Harvey <sean@silverstripe.com>
2009-07-13 23:17:22 +0000

    BUGFIX Removed orderby in GroupSubsites decorator that applies the same SubsiteID in the order by clause

--- code/GroupSubsites.php
+++ code/GroupSubsites.php
@@ -85,8 +85,6 @@ class GroupSubsites extends DataObjectDecorator {
 				$query->where[] = $where;
 				break;
 			}
-
-			$query->orderby = 'SubsiteID' . ($query->orderby ? ', ' : '') . $query->orderby;
 		}
 	}
 
