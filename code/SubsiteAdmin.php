45efdc80c0c0dc38f3d38db222aef33b2dc44a09
Tom Rix <tom@rixth.org>
2010-03-01 21:40:21 +0000

    MINOR subsite admin saving fix (from r90471)

--- code/SubsiteAdmin.php
+++ code/SubsiteAdmin.php
@@ -73,7 +73,7 @@ class SubsiteAdmin_CollectionController extends ModelAdmin_CollectionController 
 		
 		if(Director::is_ajax()) {
 			$recordController = new ModelAdmin_RecordController($this, $request, $subsite->ID);
-			return new HTTPResponse(
+			return new SS_HTTPResponse(
 				$recordController->EditForm()->forAjaxTemplate(), 
 				200, 
 				sprintf(
