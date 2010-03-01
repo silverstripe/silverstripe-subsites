d3e666c09a2f3b189ca5be0608a2188b762ff749
Tom Rix <tom@rixth.org>
2010-03-01 22:27:46 +0000

    MINOR: Added test for link rewriting on SubsitesVirtualPages (from r89909) (from r96322)

--- tests/SubsitesVirtualPageTest.php
+++ tests/SubsitesVirtualPageTest.php
@@ -194,4 +194,42 @@ class SubsitesVirtualPageTest extends SapphireTest {
 		$this->assertEquals($svp->MetaTitle, 'SVPTitle');
 		
 	}
+
+	function testFileLinkRewritingOnVirtualPages() {
+		// File setup
+		$this->logInWithPermssion('ADMIN');
+		touch(Director::baseFolder() . '/assets/testscript-test-file.pdf');
+
+		// Publish the source page
+		$page = $this->objFromFixture('Page', 'page1');
+		$this->assertTrue($page->doPublish());
+
+		$svp = new SubsitesVirtualPage();
+		$svp->CopyContentFromID = $page->ID;
+		$svp->write();
+		$svp->doPublish();
+			
+		// Create a virtual page from it, and publish that
+		
+		// Rename the file
+		$file = $this->objFromFixture('File', 'file1');
+		$file->Name = 'renamed-test-file.pdf';
+		
+		// Verify that the draft and publish virtual pages both have the corrected link
+		$this->assertContains('<img src="assets/renamed-test-file.pdf" />',
+			DB::query("SELECT \"Content\" FROM \"SiteTree\" WHERE \"ID\" = $svp->ID")->value());
+		$this->assertContains('<img src="assets/renamed-test-file.pdf" />',
+			DB::query("SELECT \"Content\" FROM \"SiteTree_Live\" WHERE \"ID\" = $svp->ID")->value());
+
+		// File teardown
+		$testFiles = array(
+			'/assets/testscript-test-file.pdf',
+			'/assets/renamed-test-file.pdf',
+		);
+		foreach($testFiles as $file) {
+			if(file_exists(Director::baseFolder().$file)) unlink(Director::baseFolder().$file);
+		}
+
+	}
+
 }
