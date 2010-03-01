7d9b6fa9bff8dc46183bc7e72ea502983f857382
Tom Rix <tom@rixth.org>
2010-03-01 22:34:03 +0000

    MINOR bring back in line with the TreeDropdownField JS API (from r96462)

--- javascript/SubsitesTreeDropdownField.js
+++ javascript/SubsitesTreeDropdownField.js
@@ -14,9 +14,8 @@ SubsitesTreeDropdownField.prototype = {
 	},
 	
 	ajaxGetTree: function(after) {
-		var baseURL = this.helperURLBase();
 		// Can't force value because it might be on a different subsite!
-		var ajaxURL =  baseURL+ 'gettree?forceValues=' + 0; //this.inputTag.value;
+		var ajaxURL =  this.buildURL('gettree?forceValues=' + 0); //this.inputTag.value;
 		
 		// Customised: Append subsiteid (evaluated in SubsitesVirtualPage.php)
 		ajaxURL += '&' + this.inputTag.name + '_SubsiteID=' + parseInt(this.subsiteID());
@@ -36,8 +35,7 @@ SubsitesTreeDropdownField.prototype = {
 		var ul = this.treeNodeHolder();
 		ul.innerHTML = ss.i18n._t('LOADING');
 		
-		var baseURL = this.options.dropdownField.helperURLBase();
-		var ajaxURL =  baseURL+ 'getsubtree?SubtreeRootID=' + this.getIdx();
+		var ajaxURL =  this.buildURL('getsubtree?SubtreeRootID=' + this.getIdx());
 		
 		// Find the root of the tree - this points to a list item in the tree, not the root div we actually want
 		// @todo: We should be using framework API calls to find the tree
