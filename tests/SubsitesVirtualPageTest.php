f7abf23ac79385997859df9336e688a57b077457
Tom Rix <tom@rixth.org>
2010-03-01 21:38:59 +0000

    MINOR: Added test for link rewriting on SubsitesVirtualPages (from r89909)

--- tests/SubsitesVirtualPageTest.php
+++ tests/SubsitesVirtualPageTest.php
@@ -1,7 +1,10 @@
 <?php
 
 class SubsitesVirtualPageTest extends SapphireTest {
-	static $fixture_file = 'subsites/tests/SubsiteTest.yml';
+	static $fixture_file = array(
+		'subsites/tests/SubsiteTest.yml',
+		'sapphire/tests/FileLinkTrackingTest.yml',
+	);
 	
 	// Attempt to bring main:linky to subsite2:linky
 	function testVirtualPageFromAnotherSubsite() {
@@ -24,4 +27,42 @@ class SubsitesVirtualPageTest extends SapphireTest {
 		$this->assertEquals($svp->SubsiteID, $subsite->ID);
 		$this->assertEquals($svp->Title, $linky->Title);
 	}
+
+	function testFileLinkRewritingOnVirtualPages() {
+		// File setup
+		$this->logInWithPermssion('ADMIN');
+		touch(Director::baseFolder() . '/assets/testscript-test-file.pdf');
+
+		// Publish the source page
+		$page = $this->objFromFixture('Page', 'page1');
+		$this->assertTrue($page->doPublish());
+
+		$svp = new SubsitesVirtualPage();
+		$svp->CopyContentFromID = $page->ID;
+		$svp->write();
+		$svp->doPublish();
+			
+		// Create a virtual page from it, and publish that
+		
+		// Rename the file
+		$file = $this->objFromFixture('File', 'file1');
+		$file->Name = 'renamed-test-file.pdf';
+		
+		// Verify that the draft and publish virtual pages both have the corrected link
+		$this->assertContains('<img src="assets/renamed-test-file.pdf" />',
+			DB::query("SELECT \"Content\" FROM \"SiteTree\" WHERE \"ID\" = $svp->ID")->value());
+		$this->assertContains('<img src="assets/renamed-test-file.pdf" />',
+			DB::query("SELECT \"Content\" FROM \"SiteTree_Live\" WHERE \"ID\" = $svp->ID")->value());
+
+		// File teardown
+		$testFiles = array(
+			'/assets/testscript-test-file.pdf',
+			'/assets/renamed-test-file.pdf',
+		);
+		foreach($testFiles as $file) {
+			if(file_exists(Director::baseFolder().$file)) unlink(Director::baseFolder().$file);
+		}
+
+	}
+
 }
