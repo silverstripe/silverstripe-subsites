0247765563522ef854be9fbf1d862a49c0d044c8
Sean Harvey <sean@silverstripe.com>
2013-05-01 12:35:17 +1200

    API Removed related pages functionality, this is now a separate module
    If you need this functionality still, please checkout
    github.com/silverstripe-labs/silverstripe-subsites_relatedpages

--- code/extensions/SiteTreeSubsites.php
+++ code/extensions/SiteTreeSubsites.php
@@ -6,21 +6,12 @@
 class SiteTreeSubsites extends DataExtension {
 	
 	public static $has_one=array(
-				'Subsite' => 'Subsite', // The subsite that this page belongs to
-				'MasterPage' => 'SiteTree',// Optional; the page that is the content master
-	);
-	
-	public static $has_many=array(
-				'RelatedPages' => 'RelatedPageLink'
+		'Subsite' => 'Subsite', // The subsite that this page belongs to
 	);
 	
 	public static $many_many=array(
-				'CrossSubsiteLinkTracking' => 'SiteTree' // Stored separately, as the logic for URL rewriting is different
+		'CrossSubsiteLinkTracking' => 'SiteTree' // Stored separately, as the logic for URL rewriting is different
 	);
-	
-	public static $belongs_many_many=array(
-				'BackCrossSubsiteLinkTracking' => 'SiteTree'
-		);
 	
 	public static $many_many_extraFields=array(
 		"CrossSubsiteLinkTracking" => array("FieldName" => "Varchar")
@@ -75,27 +66,6 @@ class SiteTreeSubsites extends DataExtension {
 			unset($subsitesMap[$this->owner->SubsiteID]);
 		} 
 
-		// Master page notice
-		if($this->owner->MasterPageID) {
-			$masterPage = $this->owner->MasterPage();
-			$masterNoteField = new LiteralField(
-				'MasterLink',
-				sprintf(
-					_t(
-						'SiteTreeSubsites.MasterLinkNote',
-						'<p>This page\'s content is copied from the <a href="%s" target="_blank">%s</a> master page (<a href="%s">edit</a>)</p>'
-					),
-					$masterPage->AbsoluteLink(), 
-					$masterPage->Title,
-					Controller::join_links(
-						singleton('CMSMain')->Link('show'),
-						$masterPage->ID
-					)
-				)
-			);
-			$fields->addFieldToTab('Root.Main',$masterNoteField);
-		} 
-
 		// Master page edit field (only allowed from default subsite to avoid inconsistent relationships)
 		$isDefaultSubsite = $this->owner->SubsiteID == 0 || $this->owner->Subsite()->DefaultSite;
 		if($isDefaultSubsite && $subsitesMap) {
@@ -116,22 +86,8 @@ class SiteTreeSubsites extends DataExtension {
 				)
 			);
 			$copyAction->includeDefaultJS(false);
-		} else {
-			$defaultSubsite = DataObject::get_one('Subsite', '"DefaultSite" = 1');
-			if($defaultSubsite) {
-				$fields->addFieldToTab('Root.Main',
-					$masterPageField = new SubsitesTreeDropdownField(
-						"MasterPageID", 
-						_t('VirtualPage.MasterPage', "Master page"), 
-						"SiteTree",
-						"ID",
-						"MenuTitle"
-					)
-				);
-				$masterPageField->setSubsiteID($defaultSubsite->ID);
-			}
 		}
-		
+
 		// replace readonly link prefix
 		$subsite = $this->owner->Subsite();
 		if($subsite && $subsite->ID) {
@@ -146,59 +102,6 @@ class SiteTreeSubsites extends DataExtension {
 			$urlsegment = $fields->dataFieldByName('URLSegment');
 			$urlsegment->setURLPrefix($url);
 		}
-		
-		$relatedCount = 0;
-		$reverse = $this->ReverseRelated();
-		if($reverse) $relatedCount += $reverse->Count();
-		$normalRelated = $this->NormalRelated();
-		if($normalRelated) $relatedCount += $normalRelated->Count();
-		
-		$tabName = $relatedCount ? 'Related (' . $relatedCount . ')' : 'Related';
-		$tab = $fields->findOrMakeTab('Root.Related', $tabName);
-		// Related pages
-		$tab->push(new LiteralField('RelatedNote',
-			'<p>You can list pages here that are related to this page.<br />When this page is updated, you will get a reminder to check whether these related pages need to be updated as well.</p>'));
-		$tab->push(
-			$related=new GridField('RelatedPages', 'Related Pages', $this->owner->RelatedPages(), GridFieldConfig_Base::create())
-		);
-		
-		$related->setModelClass('RelatedPageLink');
-		
-		// The 'show' link doesn't provide any useful info
-		//$related->setPermissions(array('add', 'edit', 'delete'));
-		
-		if($reverse) {
-			$text = '<p>In addition, this page is marked as related by the following pages: </p><p>';
-			foreach($reverse as $rpage) {
-				$text .= $rpage->RelatedPageAdminLink(true) . " - " . $rpage->AbsoluteLink(true) . "<br />\n";
-			}
-			$text .= '</p>';
-			
-			$tab->push(new LiteralField('ReverseRelated', $text));
-		}
-	
-	}
-	
-	/**
-	 * Returns the RelatedPageLink objects that are reverse-associated with this page.
-	 */
-	function ReverseRelated() {
-		return DataObject::get('RelatedPageLink', "\"RelatedPageLink\".\"RelatedPageID\" = {$this->owner->ID}
-			AND R2.\"ID\" IS NULL", '')
-			->innerJoin('SiteTree', "\"SiteTree\".\"ID\" = \"RelatedPageLink\".\"MasterPageID\"")
-			->leftJoin('RelatedPageLink', "R2.\"MasterPageID\" = {$this->owner->ID} AND R2.\"RelatedPageID\" = \"RelatedPageLink\".\"MasterPageID\"", 'R2');
-	}
-	
-	function NormalRelated() {
-		$return = new ArrayList();
-		$links = DataObject::get('RelatedPageLink', '"MasterPageID" = ' . $this->owner->ID);
-		if($links) foreach($links as $link) {
-			if($link->RelatedPage()->exists()) {
-				$return->push($link->RelatedPage());
-			}
-		}
-		
-		return $return->Count() > 0 ? $return : false;
 	}
 	
 	function alternateSiteConfig() {
@@ -280,6 +183,8 @@ class SiteTreeSubsites extends DataExtension {
 		$page->CheckedPublicationDifferences = $page->AddedToStage = true;
 		$subsiteID = ($subsiteID ? $subsiteID : $oldSubsite);
 		$page->SubsiteID = $subsiteID;
+
+		// MasterPageID is here for legacy purposes, to satisfy the subsites_relatedpages module
 		$page->MasterPageID = $this->owner->ID;
 		$page->write();
 
--- tests/SiteTreeSubsitesTest.php
+++ tests/SiteTreeSubsitesTest.php
@@ -57,32 +57,6 @@ class SiteTreeSubsitesTest extends BaseSubsiteTest {
 		$this->assertEquals($expected_path, $path);
 	}
 	
-	function testRelatedPages() {
-		$this->assertTrue(singleton('RelatedPageLink')->getCMSFields() instanceof FieldList);
-		
-		$importantpage = $this->objFromFixture('Page', 'importantpage');
-		$contact = $this->objFromFixture('Page', 'contact');
-		
-		$link = new RelatedPageLink();
-		$link->MasterPageID = $importantpage->ID;
-		$link->RelatedPageID = $contact->ID;
-		$link->write();
-		$importantpage->RelatedPages()->add($link);
-		$this->assertTrue(singleton('SiteTree')->getCMSFields() instanceof FieldList);
-		
-		$this->assertEquals($importantpage->NormalRelated()->Count(), 1);
-		$this->assertEquals($contact->ReverseRelated()->Count(), 1);
-		
-		$this->assertTrue($importantpage->getCMSFields() instanceof FieldList);
-		$this->assertTrue($contact->getCMSFields() instanceof FieldList);
-		
-		$this->assertEquals($importantpage->canView(), $link->canView());
-		$this->assertEquals($importantpage->canEdit(), $link->canEdit());
-		$this->assertEquals($importantpage->canDelete(), $link->canDelete());
-		$link->AbsoluteLink(true);
-		$this->assertEquals($link->RelatedPageAdminLink(), '<a href="admin/pages/edit/show/' . $contact->ID . '" class="cmsEditlink">Contact Us</a>');
-	}
-	
 	function testCanEditSiteTree() {
 		$admin = $this->objFromFixture('Member', 'admin');
 		$subsite1member = $this->objFromFixture('Member', 'subsite1member');
