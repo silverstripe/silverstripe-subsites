3c6000111c8db5ca5977b57cec2af1f7ea439574
Sean Harvey <sean@silverstripe.com>
2013-05-09 12:18:48 +1200

    BUG FileSubsites doesn't respect $disable_subsite_filter

--- code/extensions/FileSubsites.php
+++ code/extensions/FileSubsites.php
@@ -43,6 +43,8 @@ class FileSubsites extends DataExtension {
 	 * Update any requests to limit the results to the current site
 	 */
 	function augmentSQL(SQLQuery &$query) {
+		if(Subsite::$disable_subsite_filter) return;
+
 		// If you're querying by ID, ignore the sub-site - this is a bit ugly... (but it was WAYYYYYYYYY worse)
 		//@TODO I don't think excluding if SiteTree_ImageTracking is a good idea however because of the SS 3.0 api and ManyManyList::removeAll() changing the from table after this function is called there isn't much of a choice
 
