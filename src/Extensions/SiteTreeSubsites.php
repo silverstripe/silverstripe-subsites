7df982647df48ceecb7ed87b18e1b3f38120d9c1
Bart van Irsel <bart@webium.nl>
2022-07-26 10:10:14 +0200

    better for $subsite->Language

--- src/Extensions/SiteTreeSubsites.php
+++ src/Extensions/SiteTreeSubsites.php
@@ -400,11 +400,10 @@ class SiteTreeSubsites extends DataExtension
 
         $ignore_subsite_locale = Config::inst()->get(self::class, 'ignore_subsite_locale');
 
-        if ($subsite && $subsite->Language) {
-            if (!$ignore_subsite_locale && $subsite && i18n::getData()->validate($subsite->Language)) {
-                i18n::set_locale($subsite->Language);
-            }
+        if (!$ignore_subsite_locale && $subsite && i18n::getData()->validate($subsite->Language) && $subsite->Language) {
+            i18n::set_locale($subsite->Language);
         }
+
     }
 
     /**
