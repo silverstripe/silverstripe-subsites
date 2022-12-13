5f489b1df93d1424f24555ee199a2bd801f08eeb
Steve Boyd <emteknetnz@gmail.com>
2022-12-14 10:43:53 +1300

    FIX Do not show copy to subsite buttons in history viewer

--- src/Extensions/SiteTreeSubsites.php
+++ src/Extensions/SiteTreeSubsites.php
@@ -29,6 +29,7 @@ use SilverStripe\Subsites\Model\Subsite;
 use SilverStripe\Subsites\Service\ThemeResolver;
 use SilverStripe\Subsites\State\SubsiteState;
 use SilverStripe\View\SSViewer;
+use SilverStripe\VersionedAdmin\Controllers\HistoryViewerController;
 
 /**
  * Extension for the SiteTree object to add subsites support
@@ -117,10 +118,12 @@ class SiteTreeSubsites extends DataExtension
             $subsitesMap = new Map(ArrayList::create());
         }
 
+        $viewingPageHistory = Controller::has_curr() && Controller::curr() instanceof HistoryViewerController;
+
         // Master page edit field (only allowed from default subsite to avoid inconsistent relationships)
         $isDefaultSubsite = $this->owner->SubsiteID == 0 || $this->owner->Subsite()->DefaultSite;
 
-        if ($isDefaultSubsite && $subsitesMap->count()) {
+        if ($isDefaultSubsite && $subsitesMap->count() && !$viewingPageHistory) {
             $fields->addFieldToTab(
                 'Root.Main',
                 ToggleCompositeField::create(
