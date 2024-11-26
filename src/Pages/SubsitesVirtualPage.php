fe2b6b7ce8894ecc7e687cbc9b65297206604403
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2024-11-26 16:42:29 +1300

    API Update code to reflect changes in silverstripe/cms (#609)

--- src/Pages/SubsitesVirtualPage.php
+++ src/Pages/SubsitesVirtualPage.php
@@ -21,11 +21,6 @@ class SubsitesVirtualPage extends VirtualPage
 {
     private static $table_name = 'SubsitesVirtualPage';
 
-    /**
-     * @deprecated 5.4.0 use class_description instead.
-     */
-    private static $description = 'Displays the content of a page on another subsite';
-
     private static $class_description = 'Displays the content of a page on another subsite';
 
     private static $non_virtual_fields = [
